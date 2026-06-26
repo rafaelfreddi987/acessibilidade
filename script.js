2abc2a1d3640e4388dd8975f7495ed92d00c6a5c
rafaelfreddi987 <rafael.freddi@escola.pr.gov.br>
2026-06-26 12:16:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let tamanhoFonte = 18
 
+function aumentarFonte () {
+  tamanhoFonte += 2;
+  document.getElementyById('texcto').style.fontSize = tamanhoFonte + 'px';
+}
+function diminuirFonte () {
+  tamanhoFonte -= 2;
+  document.getElementyById('texto').style.fontSize = tamanho fonte + 'px';
+}
