2b3021743ca21fe801a8ee4e3dbe5130df1a1acf
rafaelfreddi987 <rafael.freddi@escola.pr.gov.br>
2026-07-03 12:10:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// Corrigido o nome da variável e tamanho inicial condizente com o CSS
 let tamanhoFonte = 16; 
 
 function aumentarFonte() { 
-    if (tamanhoFonte < 30) { // Limite máximo para não quebrar o layout
+    if (tamanhoFonte < 30) { // Evita que a fonte fique grande demais
         tamanhoFonte += 2;
-        // Corrigido para getElementById e fontSize
         document.getElementById('texto').style.fontSize = tamanhoFonte + 'px';
     }
 }
 
-function disminuirFonte() { 
-    if (tamanhoFonte > 12) { // Limite mínimo para continuar legível
+function diminuirFonte() { // Função corrigida e testada
+    if (tamanhoFonte > 12) { // Evita que a fonte fique pequena demais
         tamanhoFonte -= 2;
         document.getElementById('texto').style.fontSize = tamanhoFonte + 'px';
     }
 }
 
 function altoContraste() {
-    // Liga e desliga a classe de alto contraste no body
     document.body.classList.toggle('contraste');
 }
